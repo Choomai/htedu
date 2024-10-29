02a9e79357dfa1536426fab3f5086b7102573e4e
Choomai <nguyencaonguyen0944322545@gmail.com>
2024-10-29 22:42:55 +0700

    Init with Lucia auth stuff

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div class="main">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <h2>Okay</h2>
+</div>
+
+<style>
+    div.main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
