b64d91b27fc954b6a32ff2bf0f30792967b9c9ef
Choomai <nguyencaonguyen0944322545@gmail.com>
2024-11-18 22:32:50 +0700

    navigation stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<nav class="navbar">
+    <div>
+        <img src="" alt="Site logo">
+        <span>Huong Tra</span>
+    </div>
+
+    <div class="search">
+        <input type="text" id="search" placeholder="Tìm kiếm...">
+    </div>
+</nav>
+
+<nav class="sidebar">
+    <a href="/">Trang chủ</a>
+    <a href="/khu-hoc-tap">Khu học tập</a>
+    <a href="/luyen-de">Luyện đề</a>
+    <a href="/teachers">Giáo viên</a>
+    <a href="/community">Cộng đồng</a>
+    <div class="sidebar-bottom">
+        <a href="/settings">Cài đặt</a>
+    </div>
+</nav>
+<slot/>
+
+<style>
+    nav.navbar {
+        display: flex;
+    }
+
+    nav.sidebar {
+        height: 100%;
+        width: 300px;
+        border-right: 1px solid #fff;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<h3>{data.auth}</h3>
 <p>{#if data.auth}
     cmm
     <a href="/del-data">del session</a>
