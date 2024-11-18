932e08746f0b98343cb1f4fd9ab902e610b2ed0c
Choomai <nguyencaonguyen0944322545@gmail.com>
2024-11-18 23:07:31 +0700

    basic nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,61 @@
-<nav class="navbar">
-    <div>
-        <img src="" alt="Site logo">
-        <span>Huong Tra</span>
-    </div>
-
-    <div class="search">
-        <input type="text" id="search" placeholder="Tìm kiếm...">
-    </div>
-</nav>
-
 <nav class="sidebar">
-    <a href="/">Trang chủ</a>
-    <a href="/khu-hoc-tap">Khu học tập</a>
-    <a href="/luyen-de">Luyện đề</a>
-    <a href="/teachers">Giáo viên</a>
-    <a href="/community">Cộng đồng</a>
+    <div class="sidebar-main">
+        <div>
+            <img src="" alt="Site logo">
+            <span>Huong Tra</span>
+        </div>
+        <a href="/"><i class="fa-solid fa-house fa-fw"></i>Trang chủ</a>
+        <a href="/khu-hoc-tap"><i class="fa-solid fa-pen fa-fw"></i>Khu học tập</a>
+        <a href="/luyen-de"><i class="fa-solid fa-file-lines fa-fw"></i>Luyện đề</a>
+        <a href="/teachers"><i class="fa-solid fa-chalkboard-user fa-fw"></i>Giáo viên</a>
+        <a href="/community"><i class="fa-solid fa-users fa-fw"></i>Cộng đồng</a>
+    </div>
+
     <div class="sidebar-bottom">
-        <a href="/settings">Cài đặt</a>
+        <a href="/settings"><i class="fa-solid fa-gear fa-fw"></i>Cài đặt</a>
     </div>
 </nav>
-<slot/>
+
+<div class="container">
+    <nav class="navbar">
+        <input type="text" id="search" placeholder="Tìm kiếm...">
+        <div class="user">
+            <button class="fake" type="button"><i class="fa-solid fa-bell fa-2x"></i></button>
+            <img src alt="profile">
+        </div>
+    </nav>
+    <slot/>
+</div>
 
 <style>
-    nav.navbar {
+    nav.sidebar {
         display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100vh;
+        width: 300px;
+        padding: 1rem;
+        border-right: 1px solid #fff;
+    }
+    nav.sidebar a {
+        display: flex;
+        align-items: center;
+        gap: .5rem;
+        text-decoration: none;
     }
 
-    nav.sidebar {
-        height: 100%;
-        width: 300px;
-        border-right: 1px solid #fff;
+    nav.navbar {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        height: fit-content;
+        width: 100%;
+    }
+    input#search {
+        width: 200px;
+        padding: .5rem;
+    }
+    div.user {
+        justify-self: flex-end;
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,15 @@
     export let data;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>{#if data.auth}
-    cmm
-    <a href="/del-data">del session</a>
-{:else}
-    cc
-    <a href="/set-data">set session</a>
-{/if}
-</p>
+<main>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <p>{#if data.auth}
+        cmm
+        <a href="/del-data">del session</a>
+    {:else}
+        cc
+        <a href="/set-data">set session</a>
+    {/if}
+    </p>
+</main>
