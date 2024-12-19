bb60a2755f5d82ac6042fb4e240ad40a1d6314c5
Choomai <nguyencaonguyen0944322545@gmail.com>
2024-12-19 22:40:11 +0700

    use redis store

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,15 @@
 import { sveltekitSessionHandle } from "svelte-kit-sessions";
-import { SESSION_KEY } from "$env/static/private";
+import { SESSION_KEY, REDIS_HOST, REDIS_PASSWORD } from "$env/static/private";
+import { Redis } from "ioredis"
+import RedisStore from "svelte-kit-connect-redis";
+
+const client = new Redis({
+    host: REDIS_HOST,
+    password: REDIS_PASSWORD
+})
 
 export const handle = sveltekitSessionHandle({
     name: "SESSIONID",
-    secret: SESSION_KEY
+    secret: SESSION_KEY,
+    store: new RedisStore({ client })
 });
