b0f4fa527cd3a5cbc044790b967a6d041ad49fcd
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-01-12 10:40:41 +0700

    removed

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,15 +1,7 @@
 import { sveltekitSessionHandle } from "svelte-kit-sessions";
-import { SESSION_KEY, REDIS_HOST, REDIS_PASSWORD } from "$env/static/private";
-import { Redis } from "ioredis"
-import RedisStore from "svelte-kit-connect-redis";
-
-const client = new Redis({
-    host: REDIS_HOST,
-    password: REDIS_PASSWORD
-})
+import { SESSION_KEY } from "$env/static/private";
 
 export const handle = sveltekitSessionHandle({
     name: "SESSIONID",
-    secret: SESSION_KEY,
-    store: new RedisStore({ client })
+    secret: SESSION_KEY
 });
