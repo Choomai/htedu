9162d4838731c3e619e00f21124cd6c4adc67c51
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-01-21 18:18:58 +0700

    Revert "1 file?"
    This reverts commit 35a3023307e782d71c1b0aa8175502d954f848d1.

---
+++ src/lib/const.js
@@ -0,0 +1,2 @@
+export const usernamePattern = /[\w.]{3,24}/i;
+export const app_name = "Huong Tra";
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,1 @@
 // place files you want to import through the `$lib` alias in this folder.
-import { createPool } from "mysql2/promise";
-import { DB_NAME, DB_HOST, DB_SOCKET, DB_USER, DB_PASSWORD } from "$env/static/private";
-
-export const pool = createPool({
-    host: DB_HOST,
-    socketPath: DB_SOCKET,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME   
-});
-
-export const usernamePattern = /[a-z0-9._]{3,24}/i;
-export const app_name = "Huong Tra";
