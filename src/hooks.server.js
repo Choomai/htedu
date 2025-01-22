5cd03bbdc6c15b81b896ae79eeac2136b2609677
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-01-22 20:21:36 +0700

    two-way TLS is the default, more secure i guess so, but this app is not that important

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,18 +3,10 @@ import RedisStore from "svelte-kit-connect-redis";
 import fs from "node:fs";
 import { Redis } from "ioredis";
 import { REDIS_HOST, REDIS_PASSWORD, SESSION_KEY } from "$env/static/private";
-
-const cert = fs.readFileSync("cert.pem");
-const key = fs.readFileSync("key.pem");
-const ca = fs.readFileSync("ca.pem");
-
 const client = new Redis({
     host: REDIS_HOST,
     password: REDIS_PASSWORD,
-    tls: {
-        cert, key, ca,
-        rejectUnauthorized: false,
-    }
+    tls: { rejectUnauthorized: false }
 })
 
 export const handle = sveltekitSessionHandle({
