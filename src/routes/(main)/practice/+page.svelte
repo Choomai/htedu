2904beac66a426da7d000148c3f2dd6a2061df7b
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-02-05 19:50:26 +0700

    doesn't need permission

--- src/routes/(main)/practice/+page.svelte
+++ src/routes/(main)/practice/+page.svelte
@@ -15,9 +15,7 @@
     {#each data.exam_categories as category}
         <a class="normalize" href={`#${category.id}`}><i class="fa-solid fa-pen"></i>{category.title}</a>
     {/each}
-    {#if data.session.permission_level >= 1}
-        <a class="button new" href="/practice/new"><i class="fa-solid fa-plus"></i>Tạo đề</a>
-    {/if}
+    <a class="button new" href="/practice/new"><i class="fa-solid fa-plus"></i>Tạo đề</a>
 </nav>
 <main>
     <figure>
