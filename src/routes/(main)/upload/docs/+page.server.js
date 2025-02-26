e4bedbfcb019f454c2d2da94a21ed619a0fc0d20
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-02-26 21:17:56 +0700

    correct db type

--- src/routes/(main)/upload/docs/+page.server.js
+++ src/routes/(main)/upload/docs/+page.server.js
@@ -20,17 +20,17 @@ export const actions = {
             thumbnailImg = data.get("thumbnail"),
             docsName = data.get("name"),
             docsCat = data.get("category");
-        let docsFilePath = null, thumbnailImgPath = null;
+        let docsFilePath = null, thumbnailPath = null;
         const UUID = crypto.randomUUID();
 
         if (thumbnailImg instanceof File && thumbnailImg.type.startsWith("image/")) {
             const imgBuff = await thumbnailImg.arrayBuffer();
-            thumbnailImgPath = path.join(
+            thumbnailPath = path.join(
                 process.cwd(),
                 process.env.NODE_ENV == "production" ? "client" : "static",
                 "docs", `${username}@${UUID}.webp`);
-            await sharp(Buffer.from(imgBuff)).toFormat("webp").toFile(thumbnailImgPath);
-            thumbnailImgPath = `/avatars/${username}@${UUID}.webp`;
+            await sharp(Buffer.from(imgBuff)).toFormat("webp").toFile(thumbnailPath);
+            thumbnailPath = `/avatars/${username}@${UUID}.webp`;
         } else return { success: false, message: "Hình ảnh không hợp lệ" };
 
         if (docsFile instanceof File) {
@@ -45,8 +45,8 @@ export const actions = {
             docsFilePath = `/docs/${username}@${UUID}.${fileType}`;
         } else return { success: false, message: "Tài liệu không hợp lệ" };
 
-        const [rows] = await pool.execute("INSERT INTO docs(username, name, path, category) VALUES(?, ?, ?, ?)",
-            [username, docsName, docsFilePath, docsCat]);
+        const [rows] = await pool.execute("INSERT INTO docs(uuid, username, name, path, imgPath, category) VALUES(?, ?, ?, ?, ?)",
+            [UUID, username, docsName, docsFilePath, thumbnailPath, docsCat]);
         if (rows.affectedRows == 0) return { success: false, message: "Có lỗi xảy ra khi upload tài liệu" };
         return { success: true, message: "Đăng tài liệu thành công", viewPath: docsFilePath };
     }
