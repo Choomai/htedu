f0f615dcbc190caf702686916743fb1ace6d1e99
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-02-27 18:23:27 +0700

    501 not 503, also remove the adapter-auto

--- src/routes/501/+page.js
+++ src/routes/501/+page.js
@@ -2,5 +2,5 @@ import { error } from "@sveltejs/kit";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-    error(503, "Not implemented");
+    error(501, "Not Implemented");
 }
