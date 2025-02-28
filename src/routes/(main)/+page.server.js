4c0528dd7055182cf7820deee55b9f7bab822810
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-02-28 16:10:00 +0700

    match with db

--- src/routes/(main)/+page.server.js
+++ src/routes/(main)/+page.server.js
@@ -5,13 +5,15 @@ export async function load({ locals }) {
     const { session } = locals;
     const [teachers] = await pool.execute("SELECT * FROM users WHERE permission_level = 1 ORDER BY RAND() LIMIT 4");
     let stats = [null];
-    if (session.data.permission_level >= 1) [stats] = await pool.execute(`
-        SELECT
+    if (session.data.permission_level >= 1) [stats] = await pool.execute(
+        `SELECT
             COUNT(follows.follow_user_id) AS followers_count,
             (SELECT COUNT(articles.id) FROM articles WHERE user_id = ?) AS articles_count,
-            (SELECT COUNT(assignment.id) FROM assignments WHERE user_id = ?) AS assignments_count
+            (SELECT COUNT(assignments.uuid) FROM assignments WHERE user_id = ?) AS assignments_count
         FROM follows
-        WHERE follow_user_id = ?`.replace(/\s+/g, " ").trim(), [session.data.id, session.data.id, session.data.id]);
+        WHERE follow_user_id = ?`.replace(/\s+/g, " ").trim(), 
+        [session.data.id, session.data.id, session.data.id]
+    );
     const [docs] = await pool.execute("SELECT docs.*, users.avatar FROM docs LEFT JOIN users ON docs.username = users.username ORDER BY RAND() LIMIT 4");
     
     return { teachers, stats: stats[0], session: session.data, docs }
