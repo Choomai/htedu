b70f3733387d40cc17b4e649490295552b86a0a7
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-02-28 22:53:54 +0700

    unused for now

--- src/routes/(main)/assignments/[uuid]/+page.js
+++ src/routes/(main)/assignments/[uuid]/+page.js
@@ -2,5 +2,5 @@ import { error } from "@sveltejs/kit";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-    error(501, "Not Implemented")
+    error(403, "Forbidden")
 }
