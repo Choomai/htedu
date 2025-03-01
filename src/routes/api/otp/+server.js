c0315f6bad27f30b2148b077d19b8458ed5a550c
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-03-01 18:50:41 +0700

    actually 429 on the server-side, and moved the console.error up, able to log error with the mail server

--- src/routes/api/otp/+server.js
+++ src/routes/api/otp/+server.js
@@ -23,16 +23,19 @@ export async function POST({ request, locals }) {
     
     // OTP code, 6 digits
     const generatedOTP = String(randomInt(1, 999999)).padStart(6, "0");
-
-    // const otpType = data.get("type");
-    // if (otpType == "")
     let username = null;
-    
+
     if (!session.data.username) {
         const [usernameRows] = await pool.execute("SELECT username FROM users WHERE email = ?", [mailTo]);
         if (usernameRows.length == 0) error(404, "Email isn't registered");
         username = usernameRows[0].username;
     }
+    
+    const [cooldownRows] = await pool.execute("SELECT TIMESTAMPDIFF(SECOND, timestamp, NOW()) AS diff FROM users WHERE username = ?", [mailTo]);
+    if (cooldownRows.length > 0 && cooldownRows[0].diff < 60) {
+        error(429, `Please wait ${60 - cooldownRows[0].diff} seconds before requesting another OTP`);
+    }
+
     await pool.execute("INSERT INTO otp(username, otp) VALUES (?, ?) ON DUPLICATE KEY UPDATE otp = ?", [session.data.username ?? username, generatedOTP, generatedOTP]);
     try {
         await mailSender.sendMail({
@@ -42,8 +45,8 @@ export async function POST({ request, locals }) {
             text: `Your OTP code is ${generatedOTP}. This code will expire in 10 minutes.`
         })
     } catch (err) {
-        error(500, "Failed to send mail");
         console.error(err);
+        error(500, "Failed to send mail");
     }
     return new Response(`Mail sent to ${mailTo}`);
 }
