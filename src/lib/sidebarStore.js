9a8d0d4d989ba525a57ad6cfacafc3317bde7660
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-03-03 23:10:21 +0700

    work on 768

--- src/lib/sidebarStore.js
+++ src/lib/sidebarStore.js
@@ -4,7 +4,7 @@ import { writable } from "svelte/store";
 const isSSR = typeof window === 'undefined';
 
 // Get the initial value based on the screen width
-let initialValue = isSSR ? true : window.innerWidth >= 768;
+let initialValue = isSSR ? true : window.innerWidth > 768;
 
 export const sidebarState = writable(initialValue);
 export const onMobile = writable(initialValue);
@@ -12,7 +12,7 @@ export const onMobile = writable(initialValue);
 // Update sidebarState when the window is resized
 if (!isSSR) {
     window.addEventListener("resize", () => {
-        sidebarState.set(window.innerWidth >= 768);
-        onMobile.set(window.innerWidth >= 768);
+        sidebarState.set(window.innerWidth > 768);
+        onMobile.set(window.innerWidth > 768);
     });
 }
