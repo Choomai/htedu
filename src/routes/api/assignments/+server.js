c79d0af92e95d7b5995752d4c3235564b30870f8
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-03-10 03:24:09 +0700

    added api for assignments

---
+++ src/routes/api/assignments/+server.js
@@ -0,0 +1,18 @@
+import { pool } from "$lib/db";
+import { json } from "@sveltejs/kit";
+import { join } from "node:path";
+import fs from "node:fs/promises";
+
+/** @type {import('./$types').RequestHandler} */
+export async function DELETE({ request, locals }) {
+    const data = await request.formData();
+    const { session } = locals;
+
+    const uuid = data.get("uuid");
+    const [thumbnailPath] = await pool.execute("SELECT img_path FROM assigments WHERE uuid = ? AND user_id = ?", [uuid, session.data.id]);
+    if (thumbnailPath.length >= 1) await fs.unlink(join(process.cwd(), "static", docsPath[0].path));
+
+    const [deleteQuery] = await pool.execute("DELETE FROM assignments WHERE uuid = ? AND user_id = ?", [uuid, session.data.id]);
+    if (deleteQuery.affectedRows == 0) return json({ success: false, message: "Unauthorized" }, { status: 403 })
+    return json({ success: true, message: "Deleted" });
+}
