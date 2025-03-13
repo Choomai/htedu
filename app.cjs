58d280f104eea12cfef466d4201d2ee35633fed8
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-03-13 22:45:36 +0700

    serve static file in production

--- app.cjs
+++ app.cjs
@@ -1,8 +1,12 @@
+const path = requiore("node:path");
 const express = require("express");
 
 const app = express();
 const app_port = process.env.PORT || 3000;
-app.use("/static", express.static("static"));
+app.use("/ass", express.static(path.join(__dirname, "static", "ass")));
+app.use("/avatars", express.static(path.join(__dirname, "static", "avatars")));
+app.use("/docs", express.static(path.join(__dirname, "static", "docs")));
+app.use("/imgs", express.static(path.join(__dirname, "static", "imgs")));
 
 (async () => {
     // let SvelteKit handle everything else, including serving prerendered pages and static assets
