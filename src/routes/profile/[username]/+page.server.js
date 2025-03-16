dc2b359f285553e231a1665672932d64e7ce343d
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-03-16 12:32:03 +0700

    forgot id

--- src/routes/profile/[username]/+page.server.js
+++ src/routes/profile/[username]/+page.server.js
@@ -4,9 +4,10 @@ import { error } from "@sveltejs/kit";
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params, locals }) {
     const { session } = locals;
-    const [user] = await pool.execute("SELECT username, name, email, avatar FROM users WHERE username = ?", [params.username]);
+    const [user] = await pool.execute("SELECT id, username, name, email, avatar FROM users WHERE username = ?", [params.username]);
     if (user.length == 0) error(404, "Not Found");
     
+    console.log(user);
     const [followed] = await pool.execute("SELECT * FROM follows WHERE user_id = ? AND follow_user_id = ?", [session.data.id, user[0].id]);
     const [following_count] = await pool.execute("SELECT COUNT(follow_user_id) AS count FROM follows WHERE user_id = ?", [user[0].id]);
     return { 
