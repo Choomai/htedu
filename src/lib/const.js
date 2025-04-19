b3fb7c01ddc4cb8fa85fdde4aa08959b4ed8df80
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-04-19 17:28:32 +0700

    fix regex, not allowing special char username

--- src/lib/const.js
+++ src/lib/const.js
@@ -1,3 +1,3 @@
-export const usernamePattern = /[\w.]{3,24}/i;
+export const usernamePattern = /^[\w.]{3,24}$/i;
 export const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 export const app_name = "Huong Tra";
